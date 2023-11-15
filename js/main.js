9fce60557305db479262cdb8a52bf05598b21608
hilla10 <hillaman592@gmail.com>
2023-11-14 22:30:00 -1100

    modified

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const modal = document.querySelector('.modal')
 const loginBtn = document.querySelector('.login-btn')
 const closeBtn = document.querySelector('.close')
+const modalScroll = document.querySelector('.modal')
+const body = document.body;
 
 loginBtn.addEventListener('click', openModal)
 closeBtn.addEventListener('click', closeModal)
@@ -8,6 +10,8 @@ window.addEventListener('click', outsideClick)
 
 function openModal() {
     modal.style.display = 'block'
+    modalScroll.classList.toggle('scroll')
+    document.body.classList.toggle('no-scroll')
 }
 
 function closeModal() {
